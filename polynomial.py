acc069b646b7cca764d9a723a7d30e1d57368dd3
jessicazhang236 <43525793+jessicazhang236@users.noreply.github.com>
2019-11-22 17:57:23 -0500

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -7,38 +7,7 @@ import argparse
 warnings.filterwarnings('ignore', message='RankWarning')
 warnings.filterwarnings('ignore', message='Polyfit may be poorly conditioned')
 
-log_list = np.array(['00f0ad9c-27c0-8bdb-1db1-2610fd01a788'])
-"""'0224916e-bb15-41bb-ef72-38a39923554a',
-'02990aeb-3649-3188-18b1-ffc47c03185c',
-'03548b25-b4bd-d3cb-7b21-a56887607ae8',
-'03d2ab0f-4dbc-8863-43cd-9071ed265189',
-'05cd1dc7-d73d-ed63-00e7-dbd51c900184',
-'067e62dc-fe8e-4109-eee9-8a70e2f037bc',
-'0696e5c3-9b1b-2754-1693-4d3c35e2c5c5',
-'0711a84c-edb3-eded-8fda-1fed9aeda4be',
-'082788fe-461e-4d30-fa94-efed1d508192',
-'08abc439-72fa-4c86-f266-954226e45877',
-'08c94aea-2b8d-40f3-d90b-8f6a732a296b',
-'09d9227e-08bd-dfc1-1a75-33a1953c2cc3',
-'0d367004-cd84-4946-fc72-a40d7c0aedb3',
-'0df8eef4-4352-86f5-07c3-317ba54348b4',
-'0f88ddc8-c2c0-4f44-1fc1-4e59310912be',
-'10f8e3b9-5ab7-5e56-97e0-4701a56965e3',
-'15ecd6e5-495e-747e-9d79-ebe6806f4fd3',
-'18257908-e088-4a08-c6cb-c7bbd2a47922',
-'239b269e-e15b-c7f6-3d26-e4df89497014',
-'27581d62-01d2-5cc6-462f-4440435f9fc5',
-'286310ad-bb7e-cefd-ebdf-c6d9e4715417',
-'2b998b1d-50b7-9d81-a898-7a44f0230572',
-'2bd6e8d5-112f-7ea7-c248-ae8507429e38',
-'41e6c9f9-f1bb-5cfb-e923-eddd05b08464',
-432aa902-c28d-9e73-a6f5-31934a2f95d3',
-'43677200-e6f0-2051-5e6a-5e6fc3851fde',
-'4763a435-5887-4459-ee65-fb6075217a35',
-'4853043c-065a-462f-c9f0-67f8263a0eb6',
-'5da97c51-65cd-a987-dffb-8b7599aa0c0b',
-'632c320b-1b9e-de97-663e-1f78d0082617'])
-"""
+log_list = []
 def evaluate(coeff, x):
     # print(coeff)
     dim = np.size(coeff, 1)
